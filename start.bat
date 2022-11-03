04fada38c7b61678a140c934d447be2dc7be2a21
Khai Nguyen <khainx.developer@gmail.com>
2022-11-03 09:08:48 +0700

    add start.bat

---
+++ start.bat
@@ -0,0 +1,3 @@
+
+docker-compose -f ./docker-compose/docker-compose-java-stack.yml -f ./docker-compose/docker-compose.game-server.yml pull
+docker-compose -f ./docker-compose/docker-compose-java-stack.yml -f ./docker-compose/docker-compose.game-server.yml up -d
